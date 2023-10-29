b839472056f06f9a76bf33d3264b7b09d25c824b
uffiulf <sofnbeat@gmail.com>
2023-10-29 02:27:37 +0100

    la til første oppg + litt teori

---
+++ oppg1.py
@@ -0,0 +1,20 @@
+#A)
+
+# fikk hjelp fra chatgpt til å vise meg hvordan man bruker classes og følge PEP 8 og CamelCase konvensjonen.
+class Film:
+    def __init__(self, title, year, score):
+        self.title = title
+        self.year = year
+        self.score = score
+    
+    def __str__(self):
+        return f"{self.title} was released in {self.year} and currently has a score of {self.score}"
+    
+
+inception = Film("Inception", 2010, 8.8)
+the_martian = Film("The Martian", 2015, 8.0)
+joker = Film("Joker", 2019, 8.4)
+
+print(inception)
+print(the_martian)
+print(joker)
